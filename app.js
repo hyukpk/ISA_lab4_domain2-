0b74f05d48cab42e2d2651a97fa526c031d48ef7
hyukpk <hpark142@my.bcit.ca>
2024-02-17 14:22:57 -0800

    "Added comments in code to explain when GPT was used to help with assignment"

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
 const {
-    wordAlreadyExists, 
-    wordNotFoundInDictionary, 
-    wordAddedToDictionary, 
-    wordAndDefinitionError, 
-    couldNotParseJson, 
-    BadRequest, 
-    ServerListening} = require('./prompts/prompts');
+    wordAlreadyExists,
+    wordNotFoundInDictionary,
+    wordAddedToDictionary,
+    wordAndDefinitionError,
+    couldNotParseJson,
+    BadRequest,
+    ServerListening,
+} = require("./prompts/prompts");
+//Used gpt to learn how to store user facing strings in a separate file and use them
 
 let requestCount = 0;
-let dictionary = {}; // Consider replacing with persistent storage for production use
+let dictionary = {}; 
+// Used GPT to clarify suitable data structure for storing word/definition pairs
+// In this case, I decided to define it as a Javascript object due to it's support 
+// for JSON (which is what is being sent from the request in store.html)
+//
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -23,57 +29,85 @@ const server = http.createServer((req, res) => {
     // Placeholder for implementing rate limiting
     // RateLimiter(req, res);
 
-    if (req.method === 'OPTIONS') {
+    if (req.method === "OPTIONS") {
         res.writeHead(200);
         res.end();
         return;
     }
 
     function sendResponse(res, statusCode, contentType, body) {
-        res.writeHead(statusCode, {"Content-Type": contentType});
+        res.writeHead(statusCode, { "Content-Type": contentType });
         res.end(JSON.stringify(body));
     }
 
     //GET means that the user is looking for the definition to a word
-    if (req.method === "GET" && parsedUrl.pathname === '/api/definitions') {
-        
+    if (req.method === "GET" && parsedUrl.pathname === "/api/definitions") {
         const queryObject = parsedUrl.query;
         const word = queryObject.word; // Assuming the query parameter is named 'word'
-        if (word && dictionary.hasOwnProperty(word)) { // Also, using hasOwnProperty for a safer check
-            sendResponse(res, 200, "application/json", {word: word, definition: dictionary[word]});
+        if (word && dictionary.hasOwnProperty(word)) {
+            // Also, using hasOwnProperty for a safer check (GPT)
+            sendResponse(res, 200, "application/json", {
+                word: word,
+                definition: dictionary[word],
+            });
         } else {
-            sendResponse(res, 404, "application/json", {error: wordNotFoundInDictionary(requestCount, word)});
+            sendResponse(res, 404, "application/json", { //used GPT to understand error codes/when to apply them for specific errors
+                error: wordNotFoundInDictionary(requestCount, word),
+            });
         }
-        
-    // POST is when a user is trying to add a word and definition to the dictionary    
-    } else if (req.method === "POST" && parsedUrl.pathname === '/api/definitions') {
+
+        // POST is when a user is trying to add a word and definition to the dictionary
+    } else if (
+        req.method === "POST" &&
+        parsedUrl.pathname === "/api/definitions"
+    ) {
+        // Used GPT to understand what was happening in lines 65-69 
         let body = "";
-        req.on('data', chunk => {
+        req.on("data", (chunk) => {
             body += chunk.toString(); // Convert Buffer to string
         });
-        req.on('end', () => {
+        req.on("end", () => {
             try {
                 const data = JSON.parse(body);
                 // Simple input validation
-                if (data.word && typeof data.word === 'string' && data.definition && typeof data.definition === 'string') {
-                    if (data.word in dictionary) { // means the word was already defined, should throw an error
-                        sendResponse(res, 409, "application/json", {error: wordAlreadyExists(data.word)});
+                if (
+                    data.word &&
+                    typeof data.word === "string" &&
+                    data.definition &&
+                    typeof data.definition === "string"
+                ) {
+                    if (data.word in dictionary) {
+                        // means the word was already defined, should throw an error
+                        sendResponse(res, 409, "application/json", {
+                            error: wordAlreadyExists(data.word),
+                        });
                         return;
                     }
                     dictionary[data.word] = data.definition;
                     requestCount++;
                     dictionary_word_count = Object.keys(dictionary).length;
-                    sendResponse(res, 200, "application/json", {message: wordAddedToDictionary(requestCount, data.word, data.definition, dictionary_word_count)});
+                    sendResponse(res, 200, "application/json", {
+                        message: wordAddedToDictionary(
+                            requestCount,
+                            data.word,
+                            data.definition,
+                            dictionary_word_count
+                        ),
+                    });
                 } else {
-                    sendResponse(res, 400, "application/json", {error: wordAndDefinitionError});
+                    sendResponse(res, 400, "application/json", {
+                        error: wordAndDefinitionError,
+                    });
                 }
             } catch (e) {
-                sendResponse(res, 400, "application/json", {error: couldNotParseJson});
+                sendResponse(res, 400, "application/json", {
+                    error: couldNotParseJson,
+                });
             }
         });
     } else {
         // Handle unsupported methods or endpoints
-        sendResponse(res, 405, "application/json", {error: BadRequest});
+        sendResponse(res, 405, "application/json", { error: BadRequest });
     }
 });
 
